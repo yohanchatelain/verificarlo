1db8728304db68ad6cd406370691ee7a265d2a04
yohanchatelain <yohan.chatelain@gmail.com>
2019-11-12 22:06:39 +0100

    Add test for --debug-binary mode the interflop_ieee backend

---
+++ tests/test_debug_print/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#set -e
+
+check_success() {
+    if  [[ $? != 0 ]]; then
+	echo "Test failed"
+	exit 1
+    fi	
+}
+
+compile() {
+    TYPE=$1
+    verificarlo -D${TYPE^^} -O0 test.c --verbose -o test -lm
+    check_success
+}
+
+run() {
+    TYPE=$1
+    IFS=" "    
+    rm -f log.debug.binary
+    while read x y op; do
+	./test "$x" "$y" "${op}" 2>> log.debug.binary
+	echo "$x" "$y" "${op}"
+    done < value.${TYPE,,}
+    ./generate.py ${TYPE,,} value.${TYPE,,} > ref.debug.binary
+    check_success
+}
+
+compare() {
+    diff log.debug.binary ref.debug.binary
+    check_success
+}
+
+export VFC_BACKENDS_SILENT_LOADING="TRUE"
+export VFC_BACKENDS="libinterflop_ieee.so --debug-binary --print-new-line --no-print-debug-mode"
+
+for REALTYPE in "float" "double"; do
+    compile $REALTYPE
+    run $REALTYPE
+    compare
+done
+
+echo "Test succesed"
+exit 0
