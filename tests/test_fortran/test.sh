c1d60aedef85c7d625d671f18d2c8520ea8e88c7
Yohan Chatelain <yohan.chatelain@gmail.com>
2019-11-15 18:56:19 +0000

    Modify test_fortran to test FLANG_PATH instead of DRAGONEGG_PATH

--- tests/test_fortran/test.sh
+++ tests/test_fortran/test.sh
@@ -2,7 +2,7 @@
 set -e
 
 export VFC_BACKENDS="libinterflop_mca.so"
-if grep "DRAGONEGG_PATH \"\"" ../../config.h > /dev/null; then
+if grep "FLANG_PATH \"\"" ../../config.h > /dev/null; then
 	echo "this test is not run when using --without-dragonegg"
 	exit 0
 fi
@@ -10,7 +10,7 @@ fi
 # Compile newton.f90 using MCA lib instrumentation
 
 verificarlo -c newton.f90 -o newton.o
-verificarlo newton.o -o newton
+verificarlo --fortran-linker newton.o -o newton
 
 # Check that two executions differ
 ./newton > output1
